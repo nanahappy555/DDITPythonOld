42e54e4a85e7a4bcd7d971cffca226da16a5acdf
RIM <80573832+nanahappy555@users.noreply.github.com>
2022-06-29 20:41:29 +0900

    Add files via upload

---
+++ myarr01.py
@@ -0,0 +1,11 @@
+arr = ['이순신', '홍길동', '한재웅']
+
+print(arr[0])
+print(arr[1])
+print(arr[2])
+print(arr[3]) # 이순신 arr돌고 다시 1로
+
+print(arr[-1]) # 한재웅 뒤에서부터 1
+print(arr[-2]) # 홍길동 뒤에서 2
+print(arr[-3]) # 이순신 뒤에서 3
+print(arr[-4]) # IndexError: list index out of range
